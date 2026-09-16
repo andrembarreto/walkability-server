2ec282dd2ca826b6123f439f9ee06154de3ea039
Andre Barreto <andremoratob@gmail.com>
2026-09-16 15:03:03 -0300

    feat: finished implementing save journey use case

--- src/application/save_journey/save_journey_use_case.py
+++ src/application/save_journey/save_journey_use_case.py
@@ -1,26 +1,35 @@
-from domain.journey.journey_repository_interface import JourneyRepository
-from src.domain.journey.journey import Journey
+import uuid
 
-from .save_journey_dtos import SaveJourneyInputDTO, SaveJourneyOutputDTO
+from domain.journey.journey_repository_interface import JourneyRepositoryInterface
+from domain.journey.position import Position
+from domain.journey.event import Event
+from domain.journey.journey import Journey
+
+from .save_journey_dtos import SaveJourneyInputDTO, SaveJourneyOutputDTO, PositionDTO, EventDTO
 
 class SaveJourneyUseCase:
-    def __init__(self, journey_repository: JourneyRepository):
+    def __init__(self, journey_repository: JourneyRepositoryInterface):
         self.journey_repository = journey_repository
 
     def execute(self, input: SaveJourneyInputDTO) -> SaveJourneyOutputDTO:
-        if not self._is_valid_journey_data(input):
-            raise ValueError("Invalid journey data")
-
-        journey = Journey(route=input.route, events=input.events)
+        route = [self.position_from_dto(position_dto) for position_dto in input.route]
+        events = [self.event_from_dto(event_dto) for event_dto in input.events]
+        journey = Journey(id=uuid.uuid4(), route=route, events=events)
         saved_journey = self.journey_repository.save(journey)
         if not saved_journey:
             raise Exception("Failed to save journey")
 
         return SaveJourneyOutputDTO(journey_id=saved_journey.id)
 
-    def _is_valid_journey_data(self, journey_data):
-        required_fields = ['route', 'events']
-        for field in required_fields:
-            if field not in journey_data:
-                return False
-        return True
+    def event_from_dto(self, event_dto: EventDTO) -> Event:
+        return Event(
+            id=event_dto.id,
+            position=self.position_from_dto(event_dto.position)
+        )
+
+    def position_from_dto(self, position_dto: PositionDTO) -> Position:
+        return Position(
+            latitude=position_dto.latitude,
+            longitude=position_dto.longitude,
+            timestamp=position_dto.timestamp
+        )
