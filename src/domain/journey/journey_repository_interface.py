ef38b57657e7ad06d74f1419383f97be6f791f4d
Andre Barreto <andremoratob@gmail.com>
2026-09-16 22:21:38 -0300

    feat: added method to find journey in journey repository

--- src/domain/journey/journey_repository_interface.py
+++ src/domain/journey/journey_repository_interface.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 from .journey import Journey
 
 class JourneyRepositoryInterface(ABC):
     @abstractmethod
     def save(self, journey: Journey) -> Journey:
+        pass
+
+    def find(self, journey_id: UUID) -> Journey:
         pass
--- src/infra/journey/mongodb/journey_repository.py
+++ src/infra/journey/mongodb/journey_repository.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from uuid import UUID
 
 from pymongo.database import Database
 from pymongo.errors import PyMongoError
@@ -17,4 +18,15 @@ class JourneyRepository(JourneyRepositoryInterface):
             return journey
         except PyMongoError as e:
             print(f"Error saving journey: {e}")
+            return None
+
+    def find(self, journey_id: UUID) -> Journey:
+        try:
+            journey_data = self.__collection.find_one({"id": str(journey_id)})
+            if journey_data:
+                return Journey(**journey_data)
+            else:
+                return None
+        except PyMongoError as e:
+            print(f"Error finding journey: {e}")
             return None
